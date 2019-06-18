e966895ad42f00800b6b551144c6b0cc4310f253
Alyssa Alecci <Alyssa@Alyssas-iMac.local>
2019-06-17 22:11:48 -0400

    Time counts up now

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,63 +3,61 @@ $(document).ready(function () {
     // Globals
     var rotation = [0, 90, 180, 270];
     var info = $("#info");
-    var solved = 0;
-    var unsolved = 0;
     var phase = 0;
     var isSolved = false;
     var mkContBtn = $("<button id='continue'>Continue</button>");
     var mkRestartBtn = $("<button id='restart'>Play Again</button>");
     var gameSpaceFiller = "<div class='goal-container'><div id='goal-grid'></div></div><div class='puzzle-container'><div id='puzzle-grid'></div></div>"
 
-    var time = 32;
+    var time = 0;
     var clock = undefined;
+    var timeByTiles;
 
     //puzzles
-    var puzzle1 = [270, 0, 90, 180];
-    var puzzle2 = [0, 270, 0, 180, 90, 180, 0, 270, 0];
-    var puzzle3 = [270, 270, 0, 90, 180, 90, 0, 270, 90];
-    var puzzle4 = [0, 270, 0, 270, 90, 180, 90, 180, 270, 90, 180, 0, 180, 0, 270, 90];
-    var puzzle5 = [0, 180, 90, 180, 180, 0, 180, 270, 270, 90, 0, 180, 180, 0, 270, 0];
-    var puzzle6 = [180, 270, 0, 90, 180, 270, 180, 90, 0, 270, 90, 180, 270, 0, 90, 0, 270, 180, 90, 0, 180, 270, 0, 90, 180];
-    var puzzle7 = [];
+    const definedPuzzles = 7;
+    const puzzle1 = [270, 0, 90, 180];
+    const puzzle2 = [90, 270, 0, 270, 90, 180, 180, 0, 270];
+    const puzzle3 = [0, 270, 0, 180, 90, 180, 0, 270, 0];
+    const puzzle4 = [270, 270, 0, 90, 180, 90, 0, 270, 90];
+    const puzzle5 = [0, 270, 0, 270, 90, 180, 90, 180, 270, 90, 180, 0, 180, 0, 270, 90];
+    const puzzle6 = [0, 180, 90, 180, 180, 0, 180, 270, 270, 90, 0, 180, 180, 0, 270, 0];
+    const puzzle7 = [180, 90, 180, 90, 180, 270, 0, 270, 0, 270, 90, 180, 90, 180, 90, 0, 270, 0, 270, 0, 180, 90, 180, 90, 180];
     var puzzle8 = [];
     var puzzle9 = [];
     var puzzle10 = [];
-    var allPuzzles = [0, puzzle1, puzzle2, puzzle3, puzzle4, puzzle5, puzzle6, puzzle7, puzzle8, puzzle9, puzzle10]
-    var puzzleRoll = [];
-
+    var allPuzzles = [puzzle1, puzzle2, puzzle3, puzzle4, puzzle5, puzzle6, puzzle7, puzzle8, puzzle9, puzzle10];
 
     // Functions
 
     function stopTimer() {
         clearInterval(clock);
         clock = undefined;
-        time = 0;
     }
 
-    function countDown() {
-        time--;
+    function countUp() {
+        time++;
         $("#timer").html(time + " seconds");
-        if ((time === 0) || (isSolved === true)) {
-            stopTimer();
-            startScreen();
-            $("#timer").html(time + " seconds");
-            if ((time === 0) && (isSolved === false)) {
-                unsolved++;
-            }
-        }
     }
 
     function runTimer() {
-        clock = setInterval(countDown, 1000)
+        clock = setInterval(countUp, 1000)
     }
+
 
     function startScreen() {
         $("#game-space").empty();
         if (phase === (allPuzzles.length - 1)) {
-            info.html("<p>That's all the puzzles!</p>" +
-                "<p>Let's see how you did:</p>")
-            info.append(`<div class='solved'>Solved: ${solved} </div><div class='unsolved'>Unsolved: ${unsolved}</div>`);
+            console.log("Time?: " + timeByTiles * 2);
+            if (time < (timeByTiles * 2)) {
+                info.html("***");
+            }
+            else if (time > (timeByTiles * 3)) {
+                info.html("*");
+            }
+            else {
+                info.html("**");
+            }
+            info.append("<p>That's all of the puzzles!</p>")
             info.append(mkRestartBtn);
             // start puzzle when you click continue
             $("#restart").on("click", function () {
@@ -68,87 +66,98 @@ $(document).ready(function () {
         }
         else {
             if (phase === 0) {
-                info.html("<p>Make the puzzle match " +
-                    "the image on the left before time runs out!</p>");
+                info.html(`<p>Make the puzzle match
+                    the image on the left!</p>`);
             }
             else {
-                if (isSolved === false) {
-                    info.html("<p>Too bad. " +
-                        "Continue anyway?</p>");
+                console.log("Time?: " + timeByTiles * 2);
+                if (time < (timeByTiles * 2)) {
+                    info.html("***");
+                }
+                else if (time > (timeByTiles * 3)) {
+                    info.html("*");
                 }
                 else {
-                    info.html("<p>Great job! " +
-                        "Move on to the next puzzle?</p>");
+                    info.html("**");
                 }
+                info.append("<p>Great job! " +
+                    "Move on to the next puzzle?</p>");
             }
             info.append(mkContBtn);
             // start puzzle when you click continue
             $("#continue").on("click", function () {
                 startPuzzle();
             });
-            phase++;
         }
     }; // end startScreen
 
 
     function startPuzzle() {
+        console.log("The phase is: " + phase)
+        time = 0;
         isSolved = false;
         info.empty();
-        puzzleRoll = [];
-        if (phase > 6) {
+        // puzzleRoll = [];
+        if (phase > (definedPuzzles - 1)) {
             getRandom25(allPuzzles[phase]);
         }
         $("#game-space").html(gameSpaceFiller);
-        puzzle(allPuzzles[phase], puzzleRoll, phase);
+        puzzle(allPuzzles[phase]);
     };
 
 
-    function puzzle(answerArr, puzArr, p) {
+    puzzle = (answerArr) => {
+        const randomPuzzle = [];
+        console.log("puzzle start");
         // Set game up
         isSolved = false;
-        time = (answerArr.length * 2);
-        console.log(answerArr.length);
+        timeByTiles = answerArr.length;
         runTimer();
         $("#timer").html(time + " seconds");
         //Create solved puzzle image
         for (var i = 0; i < answerArr.length; i++) {
-            $("#goal-grid").addClass("puzzle-" + p).append(
-                $("<div id='static-block' class='block-color puz-" + p + "-color'></div>").css('transform', 'rotate(' + answerArr[i] + 'deg)')
+            $("#goal-grid").addClass(`puzzle-${answerArr.length}x`).append(
+                $("<div id='static-block' class='block-color puz-" + phase + "-color'></div>").css('transform', 'rotate(' + answerArr[i] + 'deg)')
             )
         }
         //Create randomized puzzle
         for (var i = 0; i < answerArr.length; i++) {
             var r = Math.floor(Math.random() * rotation.length);
-            puzArr[i] = rotation[r];
-            $("#puzzle-grid").addClass("puzzle-" + p).append(
-                $("<div id='block' class='block-color puz-" + p + "-color'></div>").attr("value", i).css('transform', 'rotate(' + puzArr[i] + 'deg)')
+            randomPuzzle[i] = rotation[r];
+            $("#puzzle-grid").addClass(`puzzle-${answerArr.length}x`).append(
+                $("<div id='block' class='block-color puz-" + phase + "-color'></div>").attr("value", i).css('transform', 'rotate(' + randomPuzzle[i] + 'deg)')
             )
         }
-        // No self-solving puzzles!
-        checkForSolved(answerArr, puzzleRoll); {
+        //No self-solving puzzles!
+        checkForSolved(answerArr, randomPuzzle); {
             if (isSolved === true) {
-                puzArr[0] = puzArr[1] = puzArr[2] = puzArr[3] = rotation[2];
+                randomPuzzle[0] = randomPuzzle[1] = randomPuzzle[2] = randomPuzzle[3] = 0;
                 isSolved = false;
             }
         }
+
         // When you click a tile...
         $(document.body).on("click", "#block", function () {
-            if (isSolved === false) {
-                var val = $(this).attr("value");
-                var r = rotation.indexOf(puzArr[val]);
-                if (r === 3) {
-                    $(this).css('transform', 'rotate(' + rotation[0] + 'deg)');
-                    puzArr[val] = rotation[0];
-                } else {
-                    $(this).css('transform', 'rotate(' + rotation[r + 1] + 'deg)');
-                    puzArr[val] = rotation[r + 1];
-                }
-                checkForSolved(answerArr, puzzleRoll);
-                console.log(isSolved);
-                console.log(puzzleRoll.join(", "));
-            } // end if solved
+            var val = $(this).attr("value");
+            var r = rotation.indexOf(randomPuzzle[val]);
+            if (phase === 2) {console.log("I rotated and r is:" + r)}
+            if (r === 3) {
+                randomPuzzle[val] = rotation[0];
+                $(this).css('transform', 'rotate(' + rotation[0] + 'deg)');
+            } else {
+                randomPuzzle[val] = rotation[r + 1];
+                $(this).css('transform', 'rotate(' + rotation[r + 1] + 'deg)');
+            }
+
+            checkForSolved(allPuzzles[phase], randomPuzzle);
+
+            if (isSolved === true) {
+                console.log("The puzzle was solved!");
+                stopTimer();
+                // ANIMATION FOR WIN HERE
+                startScreen();
+            }
         }); // end on-click
-        return isSolved;
     };
 
     function checkForSolved(answr, puz) {
@@ -158,7 +167,7 @@ $(document).ready(function () {
             }
         }
         isSolved = true;
-        solved++;
+        phase++;
         return isSolved;
     }
 
@@ -174,11 +183,10 @@ $(document).ready(function () {
     function restartGame() {
         isSolved = false;
         phase = 0;
-        solved = 0;
-        unsolved = 0;
-        puzzleRoll = [];
+        // puzzleRoll = [];
         info.empty();
         startScreen();
+        return phase;
     }
 
 
